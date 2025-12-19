d0b941af19bbccc3a8955cb6da3b0b01b1a3f2f2
Johanna Winter <johanna.winter@gmx.de>
2025-12-19 12:05:31 +0100

    add minimal event details styling

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -1,18 +1,33 @@
-import Link from "next/link";
+import {
+  DetailsWrapper,
+  EventCard,
+  EventLinkText,
+  ShareSection,
+  UploadLink,
+  CopyLinkButton,
+} from "./StyledEventDetails";
 
 export default function EventDetails({ event, link }) {
+  function handleCopyLink() {
+    navigator.clipboard.writeText();
+  }
   return (
     <>
-      <Link href="/">Back</Link>
-      <h2>Event Overview</h2>
-      <h3>{event.title}</h3>
-      <p>{event.description}</p>
-      <p>{event.date}</p>
-      <p>Share this link with your guests:</p>
-      <Link href={link}>{link}</Link>
-      <button>Copy Link</button>
-      <h3>Event Photo Gallery</h3>
-      <p>No photos added yet.</p>
+      <DetailsWrapper>
+        <EventCard>
+          <h2>Event Details</h2>
+          <h3>{event.title}</h3>
+          <p>{event.description}</p>
+          <p>{event.date}</p>
+          <ShareSection>
+            <EventLinkText>Share this link with your guests:</EventLinkText>
+            <UploadLink href={link}>{event.title} Event</UploadLink>
+            <CopyLinkButton onClick={handleCopyLink}>Copy Link</CopyLinkButton>
+          </ShareSection>
+        </EventCard>
+        <h3>Event Photo Gallery</h3>
+        <p>No photos added yet.</p>
+      </DetailsWrapper>
     </>
   );
 }
--- src/pages/events/[eventId]/index.js
+++ src/pages/events/[eventId]/index.js
@@ -1,5 +1,7 @@
 import EventDetails from "@/components/EventDetails/EventDetails";
+import Link from "next/link";
 import { useRouter } from "next/router";
+import styled from "styled-components";
 import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -20,7 +22,24 @@ export default function EventDetailPage() {
 
   return (
     <>
+      <BackButton href="/">Back</BackButton>
       <EventDetails event={event} link={guestUploadLink} />
     </>
   );
 }
+
+export const BackButton = styled(Link)`
+  display: inline-block;
+  margin: 1rem;
+  padding: 0.5rem 1rem;
+  border-radius: 6px;
+  border: 1px solid #000;
+  background: #000;
+  color: #fff;
+  font-weight: 600;
+  text-decoration: none;
+  cursor: pointer;
+  &:hover {
+    background: #333;
+  }
+`;
