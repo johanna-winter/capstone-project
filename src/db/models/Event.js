b69d293c7412d1ff6904414357217c3ec32e67da
Johanna Winter <johanna.winter@gmx.de>
2026-01-11 23:18:52 +0100

    add uploads array to event schema

--- src/db/models/Event.js
+++ src/db/models/Event.js
@@ -4,9 +4,18 @@ const { Schema } = mongoose;
 
 const eventSchema = new Schema({
   title: { type: String, required: true, minlength: 3 },
-  imageUrl: { type: String, default: "/img/default-photo-red.png" },
+  imageUrl: { type: String, default: "/img/wdefault-photo-red.png" },
   description: { type: String },
   date: { type: Date },
+  uploads: [
+    {
+      imageUrl: { type: String, required: true },
+      imagePublicId: { type: String, required: true },
+      name: String,
+      caption: String,
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
 });
 
 const Event = mongoose.models.Event || mongoose.model("Event", eventSchema);
